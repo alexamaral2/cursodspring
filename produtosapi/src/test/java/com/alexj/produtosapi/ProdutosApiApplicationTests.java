1916107357ff7a4e7cef02e3ddf44b4cba132f3e
Alex Junior <alexjunior2018@gmail.com>
2025-09-02 16:56:44 -0300

    initial project spring

---
+++ produtosapi/src/test/java/com/alexj/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexj.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
