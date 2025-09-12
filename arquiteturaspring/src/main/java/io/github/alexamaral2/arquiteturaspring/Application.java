a418cb134db2056ac76ae2d9bc85762d320b4846
Alex Junior <alexjunior2018@gmail.com>
2025-09-12 17:36:46 -0300

    adicionando novos exemplos

--- arquiteturaspring/src/main/java/io/github/alexamaral2/arquiteturaspring/Application.java
+++ arquiteturaspring/src/main/java/io/github/alexamaral2/arquiteturaspring/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) {
@@ -33,6 +35,9 @@ public class Application {
 
         ExemploValue value = applicationContext.getBean(ExemploValue.class);
         value.imprimirVariavel();
+
+        AppProperties properties = applicationContext.getBean(AppProperties.class);
+        System.out.println(properties.getValor1());
 
 	}
 
