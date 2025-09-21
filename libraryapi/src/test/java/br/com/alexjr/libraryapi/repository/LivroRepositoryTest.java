7bac554b706164fca0e17a95444512f9e17c64b8
Alex Junior <alexjunior2018@gmail.com>
2025-09-21 00:30:46 -0300

    adicionando exemplos

--- libraryapi/src/test/java/br/com/alexjr/libraryapi/repository/LivroRepositoryTest.java
+++ libraryapi/src/test/java/br/com/alexjr/libraryapi/repository/LivroRepositoryTest.java
@@ -21,12 +21,51 @@ public class LivroRepositoryTest {
     AutorRepository autorRepository;
 
     @Test
+    void salvarAutorELivro() {
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Terceiro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980,1,2));
+
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1995, 3, 10));
+
+        Autor autorSalvo = autorRepository.save(autor);
+        livro.setAutor(autorSalvo);
+
+        livroRepository.save(livro);
+    }
+
+    @Test
+    void salvarCascadeTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Outro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980,1,2));
+
+        Autor autor = new Autor();
+        autor.setNome("João");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1995, 3, 10));
+
+        livro.setAutor(autor);
+
+        livroRepository.save(livro);
+    }
+
+    @Test
     void salvarTest() {
         Livro livro = new Livro();
         livro.setIsbn("90887-84874");
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
-        livro.setTitulo("UFO");
+        livro.setTitulo("Outro Livro");
         livro.setDataPublicacao(LocalDate.of(1980,1,2));
 
         Autor autor = autorRepository
